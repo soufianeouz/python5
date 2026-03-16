3ba6f90fb84bb1d3d6caa2494762217336d10c7e
walid <walidabdi_2018@outlook.fr>
2026-03-16 03:58:57 +0000

    save update

--- ex1/data_stream.py
+++ ex1/data_stream.py
@@ -10,19 +10,29 @@ class DataStream(ABC):
     def process_batch(self, data_batch: List[Any]) -> str:
         pass
 
-    @abstractmethod
     def filter_data(self, data_batch: List[Any], criteria: Optional[str]= None) -> List[Any]:
         pass
     
-    @abstractmethod
     def get_stats(self) -> Dict[str, Union[str, int, float]]:
         pass
 
 class SensorStream(DataStream):
-    pass
+    def process_batch(self, data_batch: List[Any]) -> str:
+        return f"Processing sensor batch: {data_batch}"
 
 class TransactionStream(DataStream):
-    pass
+    def process_batch(self, data_batch: List[Any]) -> str:
+        pass
 
 class EventStream(DataStream):
-    pass
+    def process_batch(self, data_batch: List[Any]) -> str:
+        pass
+
+
+if __name__ == "__main__":
+    print("=== CODE NEXUS - POLYMORPHIC STREAM SYSTEM ===")
+    print()
+    e = SensorStream(" SENSOR_001")
+    x = e.process_batch(["temp:22.5", "humidity:65", "pressure:1013"])
+    print(x)
+    
